b4b96076b4e7c179b33b63c1d0c339e932ad645a
David Manriquez <dmanriqu@my.chemeketa.edu>
2021-12-18 22:45:34 -0800

    intial commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+//dates
+//user city input
+//empty previous data show current selection
+//open power app
+// current weather  
+//future conditions for that city 
+//city is added to the search history local storage
+// weather conditions, the temperature, the wind speed, and the humidity,uv index
+//click on a city in the search history
